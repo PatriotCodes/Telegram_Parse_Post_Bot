a139942c84e020da1f2825772f984fb856162070
PatriotCodes <patriot130690@gmail.com>
2018-03-13 13:45:30 +0200

    Added .gitignore

--- parsers.php
+++ parsers.php
@@ -1,31 +1,29 @@
 <?php
 
-function parseDanbooru($bot,$chatID,$numPosts) {
-	$bot->sendMessage($chatID,$numPosts);
-	// $doc = new DOMDocument();
-	// $doc->loadHTMLFile('https://danbooru.donmai.us/explore/posts/popular');
-	// $links = array();
-	// foreach($doc->getElementsByTagName('a') as $link) {
-	// 	if (substr($link->getAttribute('href'),0,7) == '/posts/') {
-	// 		if ($posts > 0) {
-	// 			array_push($links,$link->getAttribute('href'));
-	// 			$posts--;
-	// 		} else {
-	// 				break;
-	// 		}
-	// 	}
-	// }
-	// $hrefs = array();
-	// foreach($links as $href) {
-	// 	$doc2 = new DOMDocument();
-	// 	$doc2->loadHTMLFile('https://danbooru.donmai.us'.$href);
-	// 	foreach($doc2->getElementsByTagName('img') as $link) {
-	// 		if (!strpos($link->getAttribute('src'), 'preview')) {
-	// 			array_push($hrefs,('https://danbooru.donmai.us'.$link->getAttribute('src')));
-	// 		}
-	// 	}
-	// }
-	// return $hrefs
+function parseDanbooru($bot,$chatID,$posts) {
+	$bot->sendMessage($chatID,$posts);
+	$doc = new DOMDocument();
+	$doc->loadHTMLFile('https://danbooru.donmai.us/explore/posts/popular');
+	$links = array();
+	foreach($doc->getElementsByTagName('a') as $link) {
+		if (substr($link->getAttribute('href'),0,7) == '/posts/') {
+			if ($posts > 0) {
+				array_push($links,$link->getAttribute('href'));
+				$posts--;
+			} else {
+					break;
+			}
+		}
+	}
+	foreach($links as $href) {
+		$doc2 = new DOMDocument();
+		$doc2->loadHTMLFile('https://danbooru.donmai.us'.$href);
+		foreach($doc2->getElementsByTagName('img') as $link) {
+			if (!strpos($link->getAttribute('src'), 'preview')) {
+				$bot->sendPhoto($chatID,'https://danbooru.donmai.us'.$link->getAttribute('src'));
+			}
+		}
+	}
 }
 
 ?>
