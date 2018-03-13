f2639b718f392700222810bf74cbfaf97aba0f7e
PatriotCodes <patriot130690@gmail.com>
2018-03-13 13:21:35 +0200

    Added .gitignore

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "vendor/autoload.php";
-require_once "parsers.php";
+//require_once "parsers.php";
 $token = "564648331:AAESJurFm1Ail3NVtXz8gs85mEvyf_807D8";
 
 $bot = new \TelegramBot\Api\Client($token);
