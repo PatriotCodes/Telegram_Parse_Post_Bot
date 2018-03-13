4fd27bff6d42fdc33d00f334b141c1d4a08c179f
PatriotCodes <patriot130690@gmail.com>
2018-03-13 14:52:10 +0200

    testing

--- index.php
+++ index.php
@@ -22,9 +22,8 @@ $bot->command('help', function ($message) use ($bot) {
 $bot->command('danbooru', function ($message) use ($bot) {
     $param = str_replace('/danbooru ', '', $message->getText());
     if ($param == '/danbooru') {
-    	$answer = 'Specify number of pics
-    	to show after command:
-    	ex.: /danbooru 5';
+    	$answer = 'Specify number of pics to show 
+    after command: ex.: /danbooru 5';
     	$bot->sendMessage($message->getChat()->getId(),$answer);
     } else {
     	parseDanbooru($bot,$message->getChat()->getId(),$param);
@@ -32,7 +31,14 @@ $bot->command('danbooru', function ($message) use ($bot) {
 });
 
 $bot->command('reddit', function ($message) use ($bot) {
-    parseReddit($bot,$message->getChat()->getId(),20);
+	$param = str_replace('/reddit ', '', $message->getText());
+    if ($param == '/reddit') {
+    	$answer = 'Specify number of pics to show 
+    after command: ex.: /reddit 5';
+    	$bot->sendMessage($message->getChat()->getId(),$answer);
+    } else {
+    	parseReddit($bot,$message->getChat()->getId(),$param);
+	}
 });
 
 
