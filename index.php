bfd385361fb86ad68e7a42e917ea98c5d66acb09
PatriotCodes <patriot130690@gmail.com>
2018-03-13 15:10:04 +0200

    testing

--- index.php
+++ index.php
@@ -34,18 +34,14 @@ $bot->command('reddit', function ($message) use ($bot) {
     	$answer = "Specify number of pics to show and topic\nafter the command: ex.: /reddit 5 memes\ntype /help reddit for more info";
     	$bot->sendMessage($message->getChat()->getId(),$answer);
     } else {
-    	$paramsList = explode(" ", $params);
-    	foreach ($paramsList as $param) {
-    	 	$bot->sendMessage($message->getChat()->getId(),$param);
-    	 }
-    	// if (count($paramsList == 2)) {
-    	// 	$picsNumber == $paramsList[0];
-    	// 	$topic = $paramsList[1];
-    	// 	parseReddit($bot,$message->getChat()->getId(),$picsNumber,$topic);
-    	// } else {
-    	// 	$answer = "Specify number of pics to show and topic\nafter the command: ex.: /reddit 5 memes\ntype /help reddit for more info";
-    	// 	$bot->sendMessage($message->getChat()->getId(),$answer);
-    	// }
+    	if (count($paramsList == 2)) {
+    		$picsNumber == $paramsList[0];
+    		$topic = $paramsList[1];
+    		parseReddit($bot,$message->getChat()->getId(),$picsNumber,$topic);
+    	} else {
+    		$answer = "Specify number of pics to show and topic\nafter the command: ex.: /reddit 5 memes\ntype /help reddit for more info";
+    		$bot->sendMessage($message->getChat()->getId(),$answer);
+    	}
 	}
 });
 
