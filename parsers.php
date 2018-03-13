672c5941067cc0c586c57af2db8cbad48d37192a
PatriotCodes <patriot130690@gmail.com>
2018-03-13 16:41:49 +0200

    testing

--- parsers.php
+++ parsers.php
@@ -47,7 +47,7 @@ function parseReddit($bot,$chatID,$posts,$topic,$order) {
 		foreach($links as $href) {
 			if (substr($href,0,5) == 'https') {
 				if (strpos($href, '.img') !== false) {
-					if (strpos($href, 'i.imgur') !== true) {
+					if (strpos($href, 'i.imgur') !== false) {
 						$bot->sendMessage($chatID,$href);
 					} else {
 						$bot->sendPhoto($chatID,$href);
