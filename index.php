776750d2228fbcd01936541b7373a872395f984d
PatriotCodes <patriot130690@gmail.com>
2018-03-13 15:24:07 +0200

    testing

--- index.php
+++ index.php
@@ -40,10 +40,9 @@ $bot->command('reddit', function ($message) use ($bot) {
     			$answer = "Specify number of pics to show and topic\nafter the command: ex.: /reddit 5 memes\ntype /help reddit for more info";
     			$bot->sendMessage($message->getChat()->getId(),$answer);
     		} else {
-    			$picsNumber == $paramsList[0];
+    			$picsNumber == intval($paramsList[0]);
     			$topic = $paramsList[1];
-    			$bot->sendMessage($message->getChat()->getId(),$topic);
-				parseReddit($bot,$message->getChat()->getId(),2,$topic);
+				parseReddit($bot,$message->getChat()->getId(),$picsNumber,$topic);
 			}
     	} else {
     		$answer = "Specify number of pics to show and topic\nafter the command: ex.: /reddit 5 memes\ntype /help reddit for more info";
